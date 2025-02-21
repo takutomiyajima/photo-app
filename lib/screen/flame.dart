c7a931472c4976fbf37da3aa3c6e26f97da244d9
takutomiyajima <takutomiyajima3820@gmail.com>
2025-02-21 12:35:23 +0900

    ファイル依存関係の編集

--- lib/screen/flame.dart
+++ lib/screen/flame.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photoapp/model/displaymodel.dart';
-import 'package:photoapp/model/postmodel.dart';
 
 class Flame extends ConsumerWidget {
   final Display post;
